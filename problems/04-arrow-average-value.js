5ba2da1a76f2a184128a133d7db9349597771f8e
Jacob <jacob.lamar16@gmail.com>
2022-05-19 13:18:25 -0700

    Third done

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -10,7 +10,16 @@ arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
-// your code here!
+const arrowAvgValue = (array) => {
+  let sum = array.reduce((accum, num) => {
+    return accum + num;
+  })
+  return sum / array.length;
+}
+
+console.log(arrowAvgValue([10, 20])); // => 15
+arrowAvgValue([2, 3, 7]); // => 4
+arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
