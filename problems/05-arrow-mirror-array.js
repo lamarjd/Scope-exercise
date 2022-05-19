8bc1c1ec19dfca4beba0238451b18f74614bb723
Cecilia Ou <cou.code@gmail.com>
2022-05-19 13:34:10 -0700

    solved #5

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -6,12 +6,48 @@ Write this function using an arrow function!
 
 Examples:
 
-arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
-arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
-// your code here
+// For Loops
+const arrowMirrorArray = (array) => {
+  let newArray = [];
 
+  for (let i = 0; i < array.length; i++){
+    let ele = array[i];
+    newArray.push(ele);
+  }
+  for (let i = array.length-1; i >= 0; i--){
+    let ele = array[i];
+    newArray.push(ele);
+  }
+  return newArray;
+}
+
+// const arrowMirrorArray = (array) => {
+//   let newArray = [];
+
+//   for (let i = 0; i < array.length; i++){
+//     let ele = array[i];
+//     newArray.push(ele);
+//   }
+
+//   let reversedArray = array.reverse();
+//   console.log("Reversed: ", reversedArray)
+
+//   newArray.concat(reversedArray);
+//   // newArray.push(reversedArray);
+
+//   return console.log(newArray);
+// }
+
+// const arrowMirrorArray = (array) => {
+//   let newArray = array.slice(0);
+
+//   return console.log(newArray.concat().reverse(array));
+// }
+
+arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
+arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
