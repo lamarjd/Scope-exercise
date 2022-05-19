fc6c35f88bf5e3bf4c60504680c5c29e347807a9
Cecilia Ou <cou.code@gmail.com>
2022-05-19 15:30:10 -0700

    solved #11

--- problems/11-lazy-adder.js
+++ problems/11-lazy-adder.js
@@ -5,25 +5,31 @@ is invoked it will again accept a number, (secondNum), and then return a functio
 When the last mentioned function is invoked with a number, (thirdNum), it will
 FINALLY return a number. See below for examples!
 
-Example 1:
+AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
+- Explain, but don't code, how you would turn your solution into a one-line
+fat-arrow function
+***********************************************************************/
+// let lazyAdder = (num1) => {
+//   return function funcTwo(num2) {
+//     return function funcThree(num3) {
+//       return num1 + num2 + num3;
+//     }
+//   }
+// }
+
+let lazyAdder = num1 => funcTwo = num2 => funcThree = num3 => num1 + num2 + num3;
+
+// Example 1:
 let firstAdd = lazyAdder(1);
 let secondAdd = firstAdd(2);
 let sum = secondAdd(3);
 console.log(sum); // prints 6
 
-Example 2:
+// Example 2:
 let func1 = lazyAdder(10);
 let func2 = func1(20);
 let total = func2(3);
 console.log(total); // prints 33
-
-AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
-- Explain, but don't code, how you would turn your solution into a one-line
-  fat-arrow function
-***********************************************************************/
-
-// your code here
-
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
