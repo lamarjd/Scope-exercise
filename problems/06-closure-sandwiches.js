1112434bf2d12ffb3bad169c0e2a78baa9e3ad9b
Jacob <jacob.lamar16@gmail.com>
2022-05-19 13:55:09 -0700

    6 done

--- problems/06-closure-sandwiches.js
+++ problems/06-closure-sandwiches.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Let's practice writing closures by creating a function named `sandwichMaker()`.
-This function will return another function that will accept a string to add 
+This function will return another function that will accept a string to add
 to the sandwich order (which will start off with a default "tomato" ingredient),
 separating each incoming ingredient with "and".
 
@@ -17,7 +17,22 @@ sandwich2("pb") // => "One sandwich with tomato and pb"
 
 ***********************************************************************/
 
-// your code here!
+function sandwichMaker() {
+  let basic = "One sandwich with tomato";
+  function addSandwich(string) {
+    return basic += " and " + string;
+  }
+  return addSandwich;
+}
+
+// let sandwich = sandwichMaker(); // => returns a function
+// sandwich("spinach") // => "One sandwich with tomato and spinach"
+// sandwich("jelly") // => "One sandwich with tomato and spinach and jelly"
+// console.log(sandwich("bread")) // => "One sandwich with tomato and spinach and jelly and bread"
+
+// // Another Sandwich:
+// let sandwich2 = sandwichMaker(); // => returns a function
+// sandwich2("pb") // => "One sandwich with tomato and pb"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
