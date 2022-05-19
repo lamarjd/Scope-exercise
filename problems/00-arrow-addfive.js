15628c98b2be6d99b8889843033ba8a30cddac6a
Jacob <jacob.lamar16@gmail.com>
2022-05-19 12:55:33 -0700

    First done

--- problems/00-arrow-addfive.js
+++ problems/00-arrow-addfive.js
@@ -15,7 +15,13 @@ let result3 = addFive(37); // returns 42
 
 ***********************************************************************/
 
-let addFive = (num) => num + 5;
+let addFive = (number) => number + 5;
+
+
+// let result1 = addFive(0); // returns 5
+// console.log(result1);
+// let result2 = addFive(10); // returns 15
+// let result3 = addFive(37); // returns 42
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
